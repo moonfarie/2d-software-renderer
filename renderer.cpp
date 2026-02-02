b2b36559c14c2b9107d217449ee744e7b7b2ca31
Moonfarie <moonfarie.cpp@proton.me>
2026-02-02 22:20:01 +0200

    feat(render): add RGBA channel index constants

--- renderer.cpp
+++ renderer.cpp
@@ -7,10 +7,10 @@ namespace render {
 Renderer::Renderer(Framebuffer& framebuffer) : framebuffer_(framebuffer) {}
 
 void Renderer::draw_pixel(int x, int y, const RGBA8& color) const {
-  framebuffer_[x_pos(x) + row(y)][0] = static_cast<std::byte>(color.R);
-  framebuffer_[x_pos(x) + row(y)][1] = static_cast<std::byte>(color.G);
-  framebuffer_[x_pos(x) + row(y)][2] = static_cast<std::byte>(color.B);
-  framebuffer_[x_pos(x) + row(y)][3] = static_cast<std::byte>(color.A);
+  framebuffer_[x_pos(x) + row(y)][RGBA::R] = static_cast<std::byte>(color.R);
+  framebuffer_[x_pos(x) + row(y)][RGBA::G] = static_cast<std::byte>(color.G);
+  framebuffer_[x_pos(x) + row(y)][RGBA::B] = static_cast<std::byte>(color.B);
+  framebuffer_[x_pos(x) + row(y)][RGBA::A] = static_cast<std::byte>(color.A);
 }
 
 int Renderer::x_pos(int x) const { return x * kBytesPerPixel; }
--- rgba8.hpp
+++ rgba8.hpp
@@ -4,6 +4,15 @@
 
 namespace render {
 
+namespace RGBA {
+
+constexpr uint8_t R = 0;
+constexpr uint8_t G = 1;
+constexpr uint8_t B = 2;
+constexpr uint8_t A = 3;
+
+}  // namespace RGBA
+
 struct RGBA8 {
   uint8_t R = 0;
   uint8_t G = 0;
