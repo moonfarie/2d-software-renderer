510724cd9983177bcd5f18a04efa06206fb00a10
Moonfarie <moonfarie.cpp@proton.me>
2026-02-04 22:20:01 +0200

    refactor(window): zero-initialize members

--- window.hpp
+++ window.hpp
@@ -15,9 +15,9 @@ class Window {
  private:
   static void on_resize();
 
-  int width_{800};
-  int height_{600};
-  const std::string title_{"2D Software Renderer"};
+  int width_{0};
+  int height_{0};
+  const std::string title_{""};
   int id_{0};
 };
 
